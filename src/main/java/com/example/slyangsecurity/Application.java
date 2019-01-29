1757c17183fac51fb5d31767129747327278f9af
slyang <slyang520@yeah.net>
2019-01-29 10:31:03 +0800

    SAVE

--- src/main/java/com/example/slyangsecurity/Application.java
+++ src/main/java/com/example/slyangsecurity/Application.java
@@ -2,12 +2,23 @@ package com.example.slyangsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowCredentials(true)
+				//.allowedHeaders()
+				.allowedMethods("GET", "POST", "DELETE", "PUT");
 	}
 
 }
