9b704b4c258d1f406653324487508e29b79cb9b4
slyang <slyang520@yeah.net>
2019-08-05 11:06:59 +0800

    unit test sqlgroup   [ berore_test, after_test ]

--- src/test/java/com/example/slyangsecurity/mybatis/MybatisCheck.java
+++ src/test/java/com/example/slyangsecurity/mybatis/MybatisCheck.java
@@ -36,6 +36,7 @@ public class MybatisCheck {
         // 全局执行一次
         System.out.println("Set up before class");
     }
+
     @Before
     public void before() {
         // 每个单元测试执行前 运行一次
@@ -50,21 +51,21 @@ public class MybatisCheck {
         System.out.println("Tear down After class");
     }
 
-    
+
     @Test
     public void featuresConfigCheck() {
         // 1.cache check
         //localCacheScope=STATEMENT
         //cacheEnabled=false
-        Assert.assertEquals("请关闭一级缓存","STATEMENT",sqlSessionFactory.getConfiguration().getLocalCacheScope().name());
-        Assert.assertEquals("请关闭二级缓存",false,sqlSessionFactory.getConfiguration().isCacheEnabled());
-        Assert.assertEquals("请开启驼峰<mapUnderscoreToCamelCase>",true,sqlSessionFactory.getConfiguration().isMapUnderscoreToCamelCase());
+        Assert.assertEquals("请关闭一级缓存", "STATEMENT", sqlSessionFactory.getConfiguration().getLocalCacheScope().name());
+        Assert.assertEquals("请关闭二级缓存", false, sqlSessionFactory.getConfiguration().isCacheEnabled());
+        Assert.assertEquals("请开启驼峰<mapUnderscoreToCamelCase>", true, sqlSessionFactory.getConfiguration().isMapUnderscoreToCamelCase());
     }
 
     @Test
     public void featuresResultMapUnderscoreToCamel() {
-       Map<String,Object> value = bcChaincodeMapper.seletMapTest();
-       Assert.assertNotNull("请开启resultType=map 返回对象属性自动驼峰",value.get("testType"));
+        Map<String, Object> value = bcChaincodeMapper.seletMapTest();
+        Assert.assertNotNull("请开启resultType=map 返回对象属性自动驼峰", value.get("testType"));
 
 
         List<Map<String, Object>> bcChaincodeList = bcChaincodeMapper.selectMaps(null);
@@ -74,13 +75,14 @@ public class MybatisCheck {
 
     @Test
     @SqlGroup({
-        @Sql("/sql/sql_logic_delete.sql")
+            @Sql("/sql/sql_logic_delete.sql"),
+            @Sql(scripts = "/sql/sql_logic_delete_clear.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     })
     public void featuresLogicDelete() {
         //删除
         bcChaincodeService.removeById(20190101);
         //查询 带删除标记条件
-        BcChaincode bcChaincode =bcChaincodeService.getById(20190101);
+        BcChaincode bcChaincode = bcChaincodeService.getById(20190101);
         Assert.assertNull(bcChaincode);
 
         //TODO 查询已经删除的数据
@@ -93,7 +95,7 @@ public class MybatisCheck {
             @Sql("/sql/sql_optimistic_lock.sql")
     })
     public void featuresOptimisticLock() {
-        
+
 
     }
 
